51ebaeef5f316078303e5eb07098f0800fc66e95
Leandro Rodrigues <leandroar01@hotmail.com>
2019-07-24 11:20:48 -0300

    Exercicios

---
+++ Aula123_Programa_TratamentoExcecao.java
@@ -0,0 +1,10 @@
+package Seção_10_Tratamento_de_exceções;
+
+public class Aula123_Programa_TratamentoExcecao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Aula123_TratamentoExcecao.java
@@ -0,0 +1,5 @@
+package Seção_10_Tratamento_de_exceções;
+
+public class Aula123_TratamentoExcecao {
+
+}
